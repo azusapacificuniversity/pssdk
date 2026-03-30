85d2e31713ef9b93e6159bbe6ed7171a8e056d73
Samuel Demirdjian <sdemirdjian@apu.edu>
2026-03-30 16:00:13 -0700

    Use a simple HashMap for non threading requirements

--- src/main/java/edu/apu/pssdk/AppServer.java
+++ src/main/java/edu/apu/pssdk/AppServer.java
@@ -1,8 +1,8 @@
 package edu.apu.pssdk;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import psft.pt8.joa.API;
